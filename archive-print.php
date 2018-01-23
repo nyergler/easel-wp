f6d8315af710d836b9575e44ac42c0cdb2f08884
Nathan Yergler <nathan@yergler.net>
2018-01-23 15:58:47 -0800

    Try to distinguish between defaults and settings

--- archive-print.php
+++ archive-print.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
             <header class="page-header">
-                <h1 class="page-title"><?php echo get_option("prints_title", "Prints") ?></h1>
+                <h1 class="page-title"><?php echo get_option("prints_title", "Printz") ?></h1>
             </header>
 
             <div class="portfolio-entry-content">
